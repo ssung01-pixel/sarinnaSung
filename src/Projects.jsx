88ecc9f5b43c13ba92fc9c7d34d4265269954a0d
Sarinna <57844555+ssung01-pixel@users.noreply.github.com>
2024-07-22 02:27:05 -0400

    added projects and links

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -4,21 +4,21 @@ const Projects = () => {
   return (
     <div className="py-40 flex flex-col">
       <Card
-        title="Card Title"
-        body="This is the body of the card. It contains some text and provides information about the card."
+        title="Munchies"
+        body="Developed a web application that allows users to select their preferred restaurants and receive recommended activities in the surrounding area by utilizing Yelp Restaurant API and Decathlon API. Created wireframes, planned use cases, and conducted user testing to ensure a seamless user experience"
         link="https://github.com/ssung01-pixel/CS411project"
       />
 
       <Card
-        title="Card Title"
+        title="Asian Impact Equity"
         body="This is the body of the card. It contains some text and provides information about the card."
         link="https://github.com/ssung01-pixel/BCNC---Asian-Impact-Equity"
       />
 
       <Card
-        title="Card Title"
+        title="Mini Facebook"
         body="This is the body of the card. It contains some text and provides information about the card."
-        link="https://github.com/ssung01-pixel/CS411project"
+        link="https://github.com/ssung01-pixel/cs108-examples"
       />
     </div>
   );

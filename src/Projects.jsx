8f5aa282a2c30c3cf3e476d5952daaf453f09da2
Sarinna <57844555+ssung01-pixel@users.noreply.github.com>
2024-07-22 02:40:55 -0400

    created blurbs for each of the projects

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -5,19 +5,19 @@ const Projects = () => {
     <div className="py-40 flex flex-col">
       <Card
         title="Munchies"
-        body="Developed a web application that allows users to select their preferred restaurants and receive recommended activities in the surrounding area by utilizing Yelp Restaurant API and Decathlon API. Created wireframes, planned use cases, and conducted user testing to ensure a seamless user experience"
+        body="This web application that allows users to select their preferred restaurants and receive recommended activities in the surrounding area by utilizing Yelp Restaurant API and Decathlon API. Created wireframes, planned use cases, and conducted user testing to ensure a seamless user experience. Languages used: React, Node.js, MySQL"
         link="https://github.com/ssung01-pixel/CS411project"
       />
 
       <Card
-        title="Asian Impact Equity"
-        body="This is the body of the card. It contains some text and provides information about the card."
+        title="Asian Impact & Equity"
+        body="This project was proposed by Boston Chinatown Neighborhood Center (BCNC) organization, is a comprehensive statistical analysis on Asian American in Massachusetts, including demographic analysis of population and community analysis of jobs, education, housing. Languages used: Python"
         link="https://github.com/ssung01-pixel/BCNC---Asian-Impact-Equity"
       />
 
       <Card
         title="Mini Facebook"
-        body="This is the body of the card. It contains some text and provides information about the card."
+        body="This is a dyanamic application that emulates Facebook. It allows you to add, create, delete friends and create status messages for each one. Languages used: Python"
         link="https://github.com/ssung01-pixel/cs108-examples"
       />
     </div>

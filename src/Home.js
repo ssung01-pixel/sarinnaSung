c9f8548eda130dfb1707f0aeca946a3822d00c36
Sarinna <57844555+ssung01-pixel@users.noreply.github.com>
2024-12-02 23:06:55 -0500

    changed blurb

--- src/Home.js
+++ src/Home.js
@@ -22,7 +22,15 @@ const Home = () => {
           with information about customers' home and auto policies. Apart from
           coding, our team also focuses on creating threat models, conducting
           spikes, developing Datadog dashboards, and collaborating to enhance
-          our team's efficiency.
+          our team's efficiency. As a Software Engineer at New Era Technology,
+          my Computer Science and Statistics foundation from Boston University
+          is critical in developing innovative software solutions. Agile
+          methodologies guide our team's approach, ensuring adaptability and
+          efficiency in our projects. Bridging my academic knowledge with
+          professional expertise, I excel in strategic problem-solving and
+          Microsoft Excel. These competencies, honed through my past role as a
+          Data Architect and experience in project management, drive the success
+          of our team's endeavors at New Era.
         </p>
         <p className="pb-10">
           I have a strong interest in full-stack development. Along with my
